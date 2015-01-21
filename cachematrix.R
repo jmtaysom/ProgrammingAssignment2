612283336e16093602a6f3599a6ab3a66c4ae4e7
jmtaysom <james.taysom@gmail.com>
2015-01-21 09:27:17 -0500

    finished assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,24 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions are designed to set a matrix and its
+## inverse into the cache to later be called out for use.
 
-## Write a short comment describing this function
+## This function caches a matrix using the set command and calls
+## it back out using the get command.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    set<-function(y){
+        x<<-y
+    }
+    get<-function()x
+    list (set = set, get = get)
 }
 
-
-## Write a short comment describing this function
+## This function caches the inverse of a matrix using the setinv
+## function. The cahce can be called using the getinv function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+    i<-NULL
+    setinv<-function(solve)i<<-solve
+    getinv<-function()i
+    list(setinv = setinv, getinv = getinv)
 }
